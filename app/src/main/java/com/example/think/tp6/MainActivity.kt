f42d0a5f05f1e8b40128d5e845203988e8ce2052
Think <ulziiburend@gmail.com>
2018-12-04 15:19:46 +0100

    first

---
+++ app/src/main/java/com/example/think/tp6/MainActivity.kt
@@ -0,0 +1,12 @@
+ddpackage com.example.think.tp6
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
